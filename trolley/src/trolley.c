b7857f8e1e7238f93bd10be71b13b04f0e6b7378
0xJoeMama <0xjoemama@gmail.com>
2024-10-21 16:42:22 +0300

    impr: added README and benchmark to the trolley problem

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -6,6 +6,14 @@
 // 4 times
 #define MAX_NUM 1000000000000000000
 
+// check if number x is within the range of valid inputs
+static bool out_of_bounds(int64_t x) { return x > MAX_NUM || x < -MAX_NUM; }
+
+static void notify_for_oob(int64_t x) {
+  fprintf(stderr,
+          "specified value %" PRId64 " is outside the range [-10^18, 10^18]\n", x);
+}
+
 int main(void) {
   // Initialize 2 stack variables which we will be using throughout our whole
   // lifecycle, instead of reallocating on every repetition
@@ -21,9 +29,12 @@ int main(void) {
     if (scanf("%" SCNd64, &left) == EOF)
       break;
 
-    // if left is out of bounds, terminate with status code 1
-    if (left > MAX_NUM || left < -MAX_NUM)
+    // if left is out of bounds, terminate with status code 1 and let the user
+    // know
+    if (out_of_bounds(left)) {
+      notify_for_oob(left);
       return 1;
+    }
 
     // User input for right
     printf("Enter the cost of going right: ");
@@ -33,13 +44,16 @@ int main(void) {
       return 1;
     }
 
-    // if right is out of bounds, terminate with status code 0
-    if (right > MAX_NUM || right < -MAX_NUM)
+    // if right is out of bounds, terminate with status code 1 and let the user
+    // know
+    if (out_of_bounds(right)) {
+      notify_for_oob(right);
       return 1;
+    }
 
     // if right is greater than or equal to left, we go left.
     // otherwise we go right
-    if (right >= left) {
+    if (left <= right) {
       printf("Go left\n");
     } else {
       printf("Go right\n");
