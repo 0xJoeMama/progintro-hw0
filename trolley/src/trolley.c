b8f97882cd3ee5079dae434e33ee2c51f808cca2
Peter Plastiras <sdi2400168@di.uoa.gr>
2024-10-27 16:02:32 +0200

    impr: clean up checks for bounds since the specification does not ask for them.

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -4,23 +4,11 @@
 
 // 10^18 is just hardcoded because I wouldn't wanna repeat this long thing like
 // 4 times
-#define MAX_NUM 1000000000000000000
-
-// check if number x is within the range of valid inputs
-static bool out_of_bounds(int64_t x) { return x > MAX_NUM || x < -MAX_NUM; }
-
-static void notify_for_oob(int64_t x) {
-  fprintf(stderr,
-          "specified value %" PRId64 " is outside the range [-10^18, 10^18]\n",
-          x);
-}
-
 int main(void) {
   // Initialize 2 stack variables which we will be using throughout our whole
   // lifecycle, instead of reallocating on every repetition
   int64_t left;
   int64_t right;
-  size_t n;
 
   // loop as long as we are given data
   for (;;) {
@@ -28,37 +16,12 @@ int main(void) {
     printf("Enter the cost of going left: ");
 
     // if left is EOF terminate successfully
-    switch (n = scanf("%" SCNd64, &left)) {
-    case EOF:
-      printf("Terminating.\n");
-      return 0;
-    case 0:
-      fprintf(stderr, "not a number\n");
-      return 1;
-    }
-
-    // if left is out of bounds, terminate with status code 1 and let the user
-    // know
-    if (out_of_bounds(left)) {
-      notify_for_oob(left);
-      return 1;
-    }
+    if (scanf("%" SCNd64, &left) != 1) break;
 
     // User input for right
     printf("Enter the cost of going right: ");
-    switch (n = scanf("%" SCNd64, &right)) {
-    case EOF:
+    if (scanf("%" SCNd64, &right) != 1) {
       printf("No right cost provided.\n");
-      return 1;
-    case 0:
-      fprintf(stderr, "not a number\n");
-      return 1;
-    }
-
-    // if right is out of bounds, terminate with status code 1 and let the user
-    // know
-    if (out_of_bounds(right)) {
-      notify_for_oob(right);
       return 1;
     }
 
